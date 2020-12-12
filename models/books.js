5e7b29575565bb97522a07514e0c70c266e05511
Julie Altman <jtskolnik@gmail.com>
2020-12-12 18:14:10 -0500

    backend routes defined and tested

--- models/books.js
+++ models/books.js
@@ -3,6 +3,36 @@ var orm = require("../config/orm.js");
 
 const books = {
     //Will fill in based on group discussion 
+    selectAll: function (cb) {
+        orm.selectAll("books", function (res) {
+            cb(res);
+        })
+    },
+    allUsers: function (cb) {
+        orm.selectAll("users", function (res) {
+            cb(res);
+        })
+    },
+    selectWhere: function(col, vals, cb){
+        orm.selectWhere("books", col, vals, function(res){
+            cb(res);
+        })
+    },
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("books", cols, vals, function (res) {
+            cb(res);
+        })
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("books", objColVals, condition, function (res) {
+            cb(res);
+        })
+    },
+    deleteOne: function (condition, cb) {
+        orm.deleteOne("books", condition, function (res) {
+            cb(res);
+        })
+    }
 }
 // Export the database functions for the controller (catsController.js).
 module.exports = books;
