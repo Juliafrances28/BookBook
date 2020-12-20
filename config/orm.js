ae19f2d96969b3591d39978ede3da769b0ab30e1
Julie Altman <jtskolnik@gmail.com>
2020-12-20 10:16:12 -0500

    fixed issue of user borrowing multiple copies of the same book by accident

--- config/orm.js
+++ config/orm.js
@@ -1,20 +1,20 @@
 // Import MySQL connection.
 const connection = require("./connection.js");
-
+ 
 function printQuestionMarks(num) {
     var arr = [];
-
+ 
     for (var i = 0; i < num; i++) {
         arr.push("?");
     }
-
+ 
     return arr.toString();
 }
-
+ 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
     var arr = [];
-
+ 
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
         var value = ob[key];
@@ -29,13 +29,13 @@ function objToSql(ob) {
             arr.push(key + "=" + value);
         }
     }
-
+ 
     // translate array of strings to a single comma-separated string
     return arr.toString();
 }
 //In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. 
 //These are the methods you will need to use in order to retrieve and store data in your database.
-
+ 
 const orm = {
     //selectAll()
     selectAll: function (tableInput, cb) {
@@ -70,16 +70,16 @@ const orm = {
     //insertOne()
     insertOne: function (table, cols, vals, cb) {
         let queryString = "INSERT INTO " + table;
-
+ 
         queryString += " (";
         queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (";
         queryString += printQuestionMarks(vals.length);
         queryString += ") ";
-
+ 
         console.log(queryString);
-
+ 
         connection.query(queryString, vals, function (err, result) {
             if (err) {
                 throw err;
@@ -89,24 +89,25 @@ const orm = {
     },
     updateOne: function (table, objColVals, condition, cb) {
         let queryString = "UPDATE " + table;
-
+ 
         queryString += " SET ";
         queryString += objToSql(objColVals);
         queryString += " WHERE ";
         queryString += condition;
-
+ 
         console.log(queryString);
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
             }
-
+ 
             cb(result);
         });
     },
-    updateOneWhere: function (table, objColVals, condition1, condition2, condition3, cb) {
+    //Limit the number of updated ones to a certain number
+    updateOneLimit: function (table, objColVals, limit, condition1, condition2, condition3, cb) {
         let queryString = "UPDATE " + table;
-
+ 
         queryString += " SET ";
         queryString += objToSql(objColVals);
         queryString += " WHERE ";
@@ -115,13 +116,36 @@ const orm = {
         queryString += condition2;
         queryString += " AND ";
         queryString += condition3;
-
+        queryString +=" LIMIT ";
+        queryString += limit +";"
+ 
         console.log(queryString);
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
             }
-
+ 
+            cb(result);
+        });
+    },
+    updateOneWhere: function (table, objColVals, condition1, condition2, condition3, cb) {
+        let queryString = "UPDATE " + table;
+ 
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition1;
+        queryString += " AND ";
+        queryString += condition2;
+        queryString += " AND ";
+        queryString += condition3;
+ 
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+ 
             cb(result);
         });
     },
@@ -130,12 +154,12 @@ const orm = {
         let queryString = "DELETE FROM " + table;
         queryString += " WHERE ";
         queryString += condition;
-
+ 
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
             }
-
+ 
             cb(result);
         });
     },
@@ -146,7 +170,7 @@ const orm = {
         queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (?,?,?,?);";
-
+ 
         connection.query(queryString, vals, function (e, result) {
             console.log(queryString, vals)
             if (e) {
@@ -154,8 +178,10 @@ const orm = {
             }
             cb(result)
         })
-
+ 
     }
 }
 // Export the orm object for the model (cat.js).
 module.exports = orm;
+ 
+
--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -3,47 +3,47 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 const isUser = require("../config/middleware/isUser")
 const isNotUser = require("../config/middleware/isNotUser")
-
+ 
 const orm = require("../config/orm.js");
-
+ 
 const books = require("../models/books.js");
 // const bcrypt = require('bcrypt');
-
-
+ 
+ 
 const router = express.Router();
-
-
+ 
+ 
 //This is required to do the google books backend stuff
 const fetch = require("node-fetch");
 const dotenv = require('dotenv');
 //require("dotenv").config();
 dotenv.config();
-
+ 
 const API_KEY = process.env.API_KEY;
-
-
+ 
+ 
 // Import the model (cat.js) to use its database functions.
 const bookbook = require("../models/books.js");
 const e = require("express");
 const passport = require("../config/passport");
-
-
+ 
+ 
 router.get("/", function (req, res) {
     console.log("Sent to home because no user was found")
     //we should not have req.user since they did not pass authentication and were redirected here 
     // console.log(req)
     res.sendFile(path.join(__dirname, "../public/html/registration.html"), { message: req.flash("test") });
-
+ 
 });
-
+ 
 //serve up home page if the user logs in
 // router.get("/home", isUser, function (req, res) {
 //     res.sendFile(path.join(__dirname, "public/index.html"));
 // });
-
+ 
 //ascynhronous library bcrypt needed.  Need async, await, try and catch
 router.post("/", async function (req, res) {
-
+ 
     try {
         //part of the 2 arguments needed to create hashed password ** I can use a number only if needed and delete the salt variable.  Default is 10
         const salt = await bcrypt.genSalt();
@@ -51,24 +51,24 @@ router.post("/", async function (req, res) {
         const scrambled = await bcrypt.hash(req.body.password, salt)
         console.log(scrambled);
         books.createUser(["first_name", "last_name", "email", "secret"], [req.body.first_name, req.body.last_name, req.body.email, scrambled], function (result) {
-
+ 
         })
-
-
-
+ 
+ 
+ 
     } catch (error) {
         if (error)
             throw error;
-
+ 
     }
     res.redirect("/login")
-
+ 
     console.log("Sent to login ")
-
-
+ 
+ 
 })
-
-
+ 
+ 
 router.get("/login", function (req, res) {
     console.log("heres login!")
     res.sendFile(path.join(__dirname, "../public/html/login.html"));
@@ -80,48 +80,48 @@ router.get("/logout", function (req, res) {
     res.redirect("/login");
     // res.sendFile(path.join(__dirname, "../public/html/login.html"));
     // console.log(req.session.passport)
-
+ 
 })
-
+ 
 router.post("/login", passport.authenticate("local", {
 }), function (req, res) {
     console.log("test")
     res.json({ user: req.user, path: '/home' })
     // res.send(req.user)
-
+ 
 })
-
-
-
+ 
+ 
+ 
 router.get("/home", isUser, function (req, res) {
     //after authenticate that happends in the post login route, the redirect to /home makes req.user available
     res.sendFile(path.join(__dirname, "../public/html/homepage.html"));
     console.log("Made it to home page!")
     console.log(req.user, "line97")
 })
-
+ 
 // router.get("/home", isUser, function (req, res) {
 //     //after authenticate that happends in the post login route, the redirect to /home makes req.user available
 //     res.sendFile(path.join(__dirname, "../public/html/homepage.html"));
 //     console.log("Made it to home page!")
 // })
-
+ 
 //Server side API calls go here
 router.get("/allbooks", function (req, res) {
     books.selectAll(function (data) {
         res.json({ books: data });
     });
 });
-
+ 
 //Presents all of the users - maybe this should be more secure?
 router.get("/api/bookUser", function (req, res) {
     books.allUsers(function (data) {
         res.json({ books: data });
     });
-
+ 
 });
-
-
+ 
+ 
 //Gets entry from table from book id
 router.get("/api/bookById/:id", function (req, res) {
     let id = req.params.id;
@@ -129,8 +129,8 @@ router.get("/api/bookById/:id", function (req, res) {
         res.json(data);
     })
 });
-
-
+ 
+ 
 //Gets entry from table from book by ownerId
 router.get("/api/bookByOwnerId/:id", function (req, res) {
     let id = req.params.id;
@@ -140,8 +140,8 @@ router.get("/api/bookByOwnerId/:id", function (req, res) {
         res.json(data);
     })
 });
-
-
+ 
+ 
 //User can mark a book as Unavailble for borrowing
 router.put("/api/bookUnavailable/:bookId", function (req, res) {
     //change availability from true to false
@@ -149,7 +149,7 @@ router.put("/api/bookUnavailable/:bookId", function (req, res) {
     
     var condition = "id = " + req.params.bookId;
     
-
+ 
     books.updateOne({
         available: false
     }, condition, function (result) {
@@ -162,25 +162,25 @@ router.put("/api/bookUnavailable/:bookId", function (req, res) {
             books.selectWhere("id", id, function (data) {
                 console.log("the data in book from the update request is \n \n \n  " + JSON.stringify(data))
                 return res.json(data)
-
+ 
             })
-
+ 
         }
     });
-
-
+ 
+ 
 });
-
-
+ 
+ 
 //User can mark a book as Availble for borrowing
 router.put("/api/bookAvailable/:bookId", function (req, res) {
     //change availability from true to false
     //change checkedout from false to true
-
-
+ 
+ 
     let condition = "id = " + req.params.bookId;
     //console.log("the condition is " + condition)
-
+ 
     books.updateOne({
         available: true
     }, condition, function (result) {
@@ -193,25 +193,25 @@ router.put("/api/bookAvailable/:bookId", function (req, res) {
             books.selectWhere("id", id, function (data) {
                 console.log("the data in book from the update request is \n \n \n  " + JSON.stringify(data))
                 return res.json(data)
-
+ 
             })
-
+ 
         }
     });
-
-
+ 
+ 
 });
-
-
-
-
+ 
+ 
+ 
+ 
 //User can mark a book as borrowed
 router.put("/api/borrow/:bookId", function (req, res) {
     //change availability from true to false
     //change checkedout from false to true
-
+ 
     let condition = "id = " + req.params.bookId;
-
+ 
     books.updateOne({
         available: false
     }, condition, function (result) {
@@ -221,7 +221,7 @@ router.put("/api/borrow/:bookId", function (req, res) {
             changeSecondOne();
         }
     });
-
+ 
     function changeSecondOne() {
         books.updateOne({
             checkedOut: true
@@ -235,14 +235,14 @@ router.put("/api/borrow/:bookId", function (req, res) {
         });
     }
 });
-
+ 
 //Can mark a book as returned
 router.put("/api/:bookId/return", function (req, res) {
     //change availability from false to true
     //change checkedout from true to false
-
+ 
     let condition = "id = " + req.params.bookId;
-
+ 
     books.updateOne({
         available: true
     }, condition, function (result) {
@@ -252,7 +252,7 @@ router.put("/api/:bookId/return", function (req, res) {
             changeSecondOne();
         }
     });
-
+ 
     function changeSecondOne() {
         books.updateOne({
             checkedOut: false
@@ -265,13 +265,13 @@ router.put("/api/:bookId/return", function (req, res) {
             }
         });
     }
-
+ 
 });
-
+ 
 //User can delete a book from their library
 router.delete("/api/:bookId/delete", function (req, res) {
     let condition = "id = " + req.params.bookId;
-
+ 
     books.deleteOne(condition, function (result) {
         if (result.affectedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
@@ -281,7 +281,7 @@ router.delete("/api/:bookId/delete", function (req, res) {
         }
     });
 });
-
+ 
 //We want to add an item to the wishlist
 router.post("/wishlist", function (req, res) {
     books.insertOneWish([
@@ -293,7 +293,7 @@ router.post("/wishlist", function (req, res) {
     })
 })
 //Google Books
-
+ 
 //When the user gives a search entry, we return the JSON from google books - get request
 router.get("/gbooks/:book", function (req, res) {
     //API_KEY will give the API key just to the server, but not to the client
@@ -302,15 +302,15 @@ router.get("/gbooks/:book", function (req, res) {
     apiURL += bookSearch;
     apiURL += "&printType=books&key="
     apiURL += API_KEY;
-
+ 
     fetch(apiURL).then(function (result) {
         return result.json();
     }).then(function (response) {
         res.json(response);
     });
-
+ 
 });
-
+ 
 //When "add to library" is clicked, we need to send the backend title, author, genre, gbooksId
 router.post("/library/new/", function (req, res) {
     books.insertOne([
@@ -323,55 +323,63 @@ router.post("/library/new/", function (req, res) {
         res.json(result);
     });
 });
-
-
+ 
+ 
 //When "request to borrow" is clicked, we check for the ISBN # in the "books" table WHERE available = true
 router.put("/borrow/:isbn", function (req, res) {
-    //First need to set available=false  where isbn=value and checkedOut = false
-    //THEN set checkedOut = true where isbn = value
+    //We want to set borrowed=true for ONE entry where available=true AND borrowed=false
     let isbn2 = req.params.isbn;
-
+ 
     let condition1 = "isbn =" + isbn2;
-    let condition2 = "available = true";
+    let condition2 = "available=true";
     let condition3 = "borrowed=false";
-
-    //First set available equal to false
-    books.updateOneWhere({
-        available: false
-    }, condition1, condition2, condition3, function (result) {
+    books.updateOneLimit({
+        borrowed:true
+    }, condition1, condition2, condition3, function(result){
         if (result.changedRows == 0) {
             return res.status(400).end();
         } else {
-            changeSecondOne();
+            res.json({ isbn: isbn2 });
         }
     });
-
-    //Change borrowed to be true
-    function changeSecondOne() {
-        books.updateOne({
-            borrowed: true
-        }, condition1, function (result) {
-            if (result.changedRows == 0) {
-                return res.status(400).end();
-            } else {
-                res.json({ isbn: isbn2 });
-            }
-        })
-    }
-
+ 
+    // //First set available equal to false
+    // books.updateOneWhere({
+    //     available: false
+    // }, condition1, condition2, condition3, function (result) {
+    //     if (result.changedRows == 0) {
+    //         return res.status(400).end();
+    //     } else {
+    //         changeSecondOne();
+    //     }
+    // });
+ 
+    // //Change borrowed to be true
+    // function changeSecondOne() {
+    //     books.updateOne({
+    //         borrowed: true
+    //     }, condition1, function (result) {
+    //         if (result.changedRows == 0) {
+    //             return res.status(400).end();
+    //         } else {
+    //             res.json({ isbn: isbn2 });
+    //         }
+    //     })
+    // }
+ 
 });
-
+ 
 //We want to be able to get all of the books that are available - maybe we should limit the number of responses?
 router.get("/books/available", function (req, res) {
     books.selectWhere("available", 1, function (data) {
         res.json(data);
     });
 })
-
-
+ 
+ 
 // API route to get logged in user's data
 router.get("/api/user_data", function (req, res) {
-
+ 
     let user;
     if (!req.user) {
         console.log("retrieving test user");
@@ -387,7 +395,7 @@ router.get("/api/user_data", function (req, res) {
             //console.log("this is the test user \n " + JSON.stringify(user))
             res.json({ user })
         });
-
+ 
     }
     else {
         // user = {
@@ -396,7 +404,7 @@ router.get("/api/user_data", function (req, res) {
         //     last_name: req.user.last_name,
         //     email: req.user.email,
         // }
-
+ 
         // books.selectUser('id', req.user[0].id, function (result) {
         books.selectUser('id', req.user[0].id, function (result) {
             console.log(result)
@@ -407,30 +415,30 @@ router.get("/api/user_data", function (req, res) {
                 last_name: result[0].last_name,
                 email: result[0].email,
             }
-
+ 
             console.log("this is the user info \n " + JSON.stringify(user))
             res.json({ user })
         })
         console.log("sent Json with User info")
-
+ 
         // })
         // res.json({ user });
         //console.log("sent Json with User info")
     }//Ends else statement
-
+ 
 })
-
+ 
 //When a book in the "available" section is clicked it is marked as borrowed
 //To mark a book as borrowed from the available list
 router.put("/available/borrow/:id", function (req, res) {
     //First need to set available=false  where isbn=value and checkedOut = false
     //THEN set checkedOut = true where isbn = value
     let id = req.params.id;
-
+ 
     let condition1 = "id=" + id;
     let condition2 = "available = true";
     let condition3 = "borrowed = false";
-
+ 
     //First set available equal to false
     books.updateOneWhere({
         available: false
@@ -441,7 +449,7 @@ router.put("/available/borrow/:id", function (req, res) {
             changeSecondOne();
         }
     });
-
+ 
     //Change borrowed to be true
     function changeSecondOne() {
         books.updateOne({
@@ -454,20 +462,20 @@ router.put("/available/borrow/:id", function (req, res) {
             }
         })
     }
-
-
+ 
+ 
 })
-
-
-
-
+ 
+ 
+ 
+ 
 //When a book from the available list is borrowed, we want to insert the userId as the borrowed id
 router.put("/insert/:borrowerId/:bookId", function (req, res) {
     let borrowerId = req.params.borrowerId;
     let bookId = req.params.bookId;
     let condition = "id = " + bookId;
-
-
+ 
+ 
     books.updateOne({
         "borrowerId": borrowerId
     }, condition, function (result) {
@@ -477,15 +485,15 @@ router.put("/insert/:borrowerId/:bookId", function (req, res) {
             res.json({ id: req.params.id });
         }
     });
-
+ 
 });
-
+ 
 //Inserts a borrowers email into the table
 router.put("/insertemail/:borrowerEmail/:bookId", function (req, res) {
     let borrowerEmail = req.params.borrowerEmail;
     let bookId = req.params.bookId;
     let condition = "id = " + bookId;
-
+ 
     books.updateOne({
         "borrowerEmail": borrowerEmail
     }, condition, function (result) {
@@ -496,12 +504,12 @@ router.put("/insertemail/:borrowerEmail/:bookId", function (req, res) {
         }
     });
 })
-
-
+ 
+ 
 /*
-
+ 
 COMMENTING THIS OUT SINCE IT'S NOT BEING USED
-
+ 
 //Gets entry from books where id = id AND borrowed = true
 router.get("/api/bookIdWhereBorrowed/:id", function (req, res) {
     let id = req.params.id;
@@ -512,19 +520,19 @@ router.get("/api/bookIdWhereBorrowed/:id", function (req, res) {
     
 });
 */
-
-
+ 
+ 
 //Returning Books will set avail to true and borrowed to false
 router.put("/api/returnBook/:id", function (req, res) {
     //First need to set available=false  where isbn=value and checkedOut = false
     //THEN set checkedOut = true where isbn = value
     let id = req.params.id;
     console.log(JSON.stringify(req.body))
-
+ 
     let condition = "id=" + id;
     //let condition2 = "available = false";
     //let condition3 = "borrowed = true";
-
+ 
     //First set available equal to true
     books.updateOne({
         borrowed: false
@@ -538,15 +546,17 @@ router.put("/api/returnBook/:id", function (req, res) {
             books.selectWhere("id", id, function (data) {
                 console.log("the data in book from the update request is \n \n \n  " + JSON.stringify(data))
                 return res.json(data)
-
+ 
             })
-
+ 
         }
     });
 })
-
-
-
-
+ 
+ 
+ 
+ 
 // Export routes for server.js to use.
 module.exports = router;
+ 
+
